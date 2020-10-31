5bee5632250b4004cce065c48fd20857940aaec0
Bi Cheng Wu <bwu62@wisc.edu>
2020-10-30 23:18:31 -0500

    added anti-gibbs detection; refactored peak area functions; added scores to plot; other minor changes

--- LymanFFT/lyman_sub.R
+++ LymanFFT/lyman_sub.R
@@ -18,16 +18,23 @@ low.pass = function(flux,thresh=200,strength=0.03,debug=FALSE){
   return(f.raw*ab[2]+ab[1])
 }
 
+# define function for finding end of peak
+falling.or.zero.bound = function(vec){
+  return(which( c(F,diff(vec)>0) | vec==0 )[1]-1)
+}
+
+# define function to get peak indices
+peak.indices = function(vector,index=NULL){
+  n = length(vector)
+  if(is.null(index)) index=which(vector==max(vector))
+  return(c(max(1,index-falling.or.zero.bound(rev(vector[1:(index-1)])),na.rm=T),
+           min(n,index+falling.or.zero.bound(vector[(index+1):n]),na.rm=T)))
+}
+
 # define normalized peak area function
 norm.peak.area = function(vector,index){
-  fallingOrZeroBound = function(vec){
-    return(which( c(F,diff(vec)>0) | vec==0 )[1]-1)
-  }
-  n = length(vector)
-  return(sum(vector[c(
-    max(1,index-fallingOrZeroBound(rev(vector[1:(index-1)])),na.rm=T):
-      min(n,index+fallingOrZeroBound(vector[(index+1):n]),na.rm=T)
-  )])/sum(vector))
+  bounds = peak.indices(vector,index=index)
+  return(sum(vector[bounds[1]:bounds[2]])/sum(vector))
 }
 
 # load template and save # rows for use later
@@ -48,9 +55,10 @@ K_scale = function(x){1-pmin(pmax(x-K_thresh,0)/(0.8-K_thresh),1)}
 
 ScoreSpec = function(spec.file){
   
+  # cat(paste0("Running ",spec.file,"\n"))
   scores = setNames(rep(NA,4),c("Area.peak","KS.raw","KS.trans","Combined"))
   
-  tryCatch({ 
+  tryCatch({
     
     # read in spectrum and check column names
     spec = readFrameFromFITS(spec.file)
@@ -61,9 +69,21 @@ ScoreSpec = function(spec.file){
     spec$flux.pass = low.pass(spec$flux)
     spec$flux.peak = loess.peak(spec$flux.pass)
     
-    # convolve with template, and get offset of max
-    conv       = convolve(spec$flux.peak,template$flux.peak,type="filter")
-    conv.max   = which.max(conv)
+    # convolve with template
+    conv = convolve(spec$flux.peak,template$flux.peak,type="filter")
+    
+    # get start and end indices of max peak of absolute value of conv
+    max.indices = peak.indices(abs(conv))
+    
+    
+    # if this max peak is directly at beginning or end of conv vector,
+    # probably due to gibbs phenomenon, so we ignore it
+    if( 0 %in% max.indices | (nrow(spec)-N+1) %in% max.indices ){
+      conv[max.indices[1]:max.indices[2]] = 0
+    }
+    
+    # get max of conv, now without absolute value (check only for positive-correlation match)
+    conv.max = which.max(conv)
     
     # transform to emphasize peaks, and find normalized area of peak at max-conv offset
     conv.trans = pmax(0,abs(conv)-conv[conv.max]*C_thresh)^2
@@ -89,13 +109,14 @@ ScoreSpec = function(spec.file){
     # get spec name
     spec.name = gsub(".*/|\\.fits?","",sub("^.*(spec[-0-9]*).*$","\\1",spec.file))
     
-    # create file for plot output 
-    png(file=paste0("plots/",spec.name,".png"),res=300,width=5,height=4,units="in")
+    # create file for plot output
+    pdf(file=paste0("plots/",spec.name,".pdf"),width=5,height=4)
     
     # make basic plot of output
     par(cex=.6,las=1)
     plot(1:N,newSpec,type='l',col='black',lwd=1,ylim=c(min(newTemp)-y.off,max(newSpec)),
-         ylab="Rescaled Flux (template offset for legibility)",xlab="Wavelength index",main=spec.name)
+         ylab="Rescaled Flux (template offset for legibility)",xlab="Wavelength index",
+         main=sprintf("%s  (A/K/C scores: %.2f %.2f %.4f)",spec.name,scores[1],scores[3],scores[4]))
     lines(1:N,newTemp-y.off*.9,type='l',lwd=1,col='blue')
     legend(x=N,y=min(newTemp)-y.off,legend=c("Spectrum","Template"),
            col=c("black","blue"),lty=1,lwd=2,cex=.8,xjust=.9,yjust=.1)
@@ -113,12 +134,12 @@ ScoreSpec = function(spec.file){
 
 
 # get fits files
-files = list.files("data/",full.names=T)
+files = list.files("data",full.names=T)
 
 # create/clear plots output directory
-if(dir.exists("plots/")){
+if(dir.exists("plots")){
   unlink("plots/*")
-} else dir.create("plots/")
+} else dir.create("plots")
 
 # run analysis, sort by score, and change rownames
 res = as.data.frame(t(sapply(files,ScoreSpec)))
